76737ffa3fae4b1ef0e2a34b1ea023d6068de800
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2025-03-27 19:09:50 +0800

    worked on responsive design for word cloud

--- src/components/WordCloud.jsx
+++ src/components/WordCloud.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import data from "../data.json"
 import Word from './Word'
 
 function WordCloud() {
 	const [scale, setScale] = useState(0);
+	const wordCloudRef = useRef(null)
 
 	const resizeScale = () => {
-		if (screen.width > 1500) setScale(1.8)
-		else if (screen.width > 1000) setScale(1.6)
-		else if (screen.width > 700) setScale(1.4)
+		if (screen.width > 1600) setScale(1.6);
+		else if (screen.width > 1400) setScale(1.4);
+		else if (screen.width > 1100) setScale(1.3);
+		else if (screen.width > 800) setScale(1.2);
+		else if (screen.width > 500) setScale(0.9);
+		else setScale(0.85);
 	}
 
 	useEffect(() => {
@@ -19,7 +23,7 @@ function WordCloud() {
 	}, [])
 
 	return (
-		<div className="word-cloud">
+		<div className="word-cloud" ref={wordCloudRef}>
 			{Object.keys(data["word-cloud"]).map((text) => (
 				<Word key={text} word={data["word-cloud"][text]} text={text} scale={scale} />
 			))}
