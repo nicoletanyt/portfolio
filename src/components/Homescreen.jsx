826533e6dc77fe8b82996f34259daf78ad7161e1
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2025-08-03 13:09:41 +0800

    changed age description

--- src/components/Homescreen.jsx
+++ src/components/Homescreen.jsx
@@ -9,7 +9,7 @@ function Homescreen({elRef}) {
 			<h1 className="name">
 				hi, i'm <span className="italic">nicole tan</span>
 			</h1>
-			<p>16 year-old student with a passion for coding, music and reading.</p>
+			<p>17 year-old student with a passion for coding, music and reading.</p>
 			<FaAngleDoubleDown className="down-icon animate__bounce animate__animated animate__infinite" />
 		</div>
 	);
